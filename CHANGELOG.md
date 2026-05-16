502dfbb0a7b9249bdd1399c103489a93208af2a6
🚀 Niklas Arens <niklas.arens.23@gmail.com>
2026-05-16 14:20:29 +0200

    Release FleetTracker v1.3.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
+
+## [1.3.2] - 2026-05-16
 ### Changed
 - Moved ObjectInfo list expansion out of FleetTracker and into the standalone UXTweaks mod.
 
--- Plugin.cs
+++ Plugin.cs
@@ -5,7 +5,7 @@ using SolarExpanseFleetTracker.UI;
 
 namespace SolarExpanseFleetTracker
 {
-    [BepInPlugin("com.mod.solarexpanse.fleettracker", "FleetTracker", "1.3.1")]
+    [BepInPlugin("com.mod.solarexpanse.fleettracker", "FleetTracker", "1.3.2")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
